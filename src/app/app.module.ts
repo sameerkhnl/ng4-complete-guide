db5fbd422c032a890677e5c516cae64d80896521
Sameer Khanal <khanal.sam91@gmail.com>
2018-06-11 17:10:19 +1200

    added shoppingList and recipe component (my own implementation)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {HeaderComponent} from '../header/header.component';
+import {HeaderComponent} from './header/header.component';
+import {ShoppingListComponent} from './shoppingList/shoppingList.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shoppingList/shoppingList.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppingList',
+  templateUrl: './shoppingList.component.html',
+  styleUrls: ['./shoppingList.component.css'],
+})
+
+export class ShoppingListComponent {
+
+}
