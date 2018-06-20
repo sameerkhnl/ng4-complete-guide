cedfadcde59f7f6cff8343297cec36e0b62e6644
Sameer Khanal <khanal.sam91@gmail.com>
2018-06-20 16:41:33 +1200

    uncommented scripts in angular.json

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {Routes} from '@angular/router';
+import {RecipeComponent} from './recipe/recipe.component';
+
+const appRoutes: Routes = [
+  {path: '', component: RecipeComponent}
+]
+
+
+@NgModule({
+
+})
+
+
+
+export class AppRoutingModule {
+
+}
