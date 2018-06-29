cd9fc68b98cbbdc2ca862a5a543c9d5c34aaed08
Sameer Khanal <khanal.sam91@gmail.com>
2018-06-29 13:52:00 +1200

    added validation

--- src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {RecipeService} from '../recipe.service';
 import {Ingredient} from '../../shared/Ingredient';
 import {Recipe} from '../recipe.model';
@@ -15,30 +15,19 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private fb: FormBuilder, private recipeService: RecipeService) { }
+  constructor(private route: ActivatedRoute, private fb: FormBuilder, private recipeService: RecipeService) {
+  }
 
   ngOnInit() {
     this.route.params.subscribe((p) => {
       console.log(p['id']);
-      if(p['id'] !== undefined){
+      if (p['id'] !== undefined) {
         this.editMode = true;
         this.id = Number(p['id']);
       }
       console.log('new mode: ' + !this.editMode);
     });
-
-    this.recipeForm = this.fb.group({
-      name: '',
-      imageUrl: '',
-      description: '',
-      ingredients: this.fb.array([]),
-    });
-
-    if(this.editMode){
-      this.setIngredients(this.recipeService.getRecipeById(this.id));
-
-
-    }
+    this.initForm();
   }
 
   onSubmit() {
@@ -46,12 +35,12 @@ export class RecipeEditComponent implements OnInit {
 
   }
 
-  get ingredients(){
+  get ingredients() {
     return this.recipeForm.get('ingredients') as FormArray;
   }
 
   onAddIngredient() {
-    this.ingredients.push(this.fb.group({name: '', amount: 0}));
+    this.ingredients.push(this.fb.group({name: ['', Validators.required], amount: [0, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)]]}));
   }
 
   removeIngredient(i: number) {
@@ -60,36 +49,55 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSave() {
-    if(this.editMode){
+    if (this.editMode) {
       const retrieved = this.recipeService.getRecipeById(this.id);
-     const formModel = this.recipeForm.value;
-     const ingredientsCopy = formModel.ingredients.map((ingredient: Ingredient) => Object.assign({}, ingredient));
-     const saveRecipe: Recipe = {
-       id: this.id,
-       name: formModel.name,
-       description: formModel.description,
-      imagePath: formModel.imageUrl,
-      ingredients: ingredientsCopy
-     }
-     retrieved.name = saveRecipe.name;
-     retrieved.description = saveRecipe.description;
-     retrieved.imagePath = saveRecipe.imagePath;
-     retrieved.ingredients = saveRecipe.ingredients;
+      const formModel = this.recipeForm.value;
+      const ingredientsCopy = formModel.ingredients.map((ingredient: Ingredient) => Object.assign({}, ingredient));
+      const saveRecipe: Recipe = {
+        id: this.id,
+        name: formModel.name,
+        description: formModel.description,
+        imagePath: formModel.imageUrl,
+        ingredients: ingredientsCopy
+      };
+      retrieved.name = saveRecipe.name;
+      retrieved.description = saveRecipe.description;
+      retrieved.imagePath = saveRecipe.imagePath;
+      retrieved.ingredients = saveRecipe.ingredients;
 
     }
   }
 
-  setIngredients(recipe: Recipe) {
-    const ingsTemp = recipe.ingredients.slice().map(i => this.fb.group({name: i.name, amount: i.amount}));
-    const ingredientsFormsArray = this.fb.array(ingsTemp);
-    this.recipeForm.setControl('ingredients', ingredientsFormsArray);
-    this.recipeForm.patchValue({
-      name: recipe.name,
-      imageUrl: recipe.imagePath,
-      description: recipe.description,
+  initForm() {
+    this.recipeForm = this.fb.group({
+      name: ['', Validators.required],
+      imageUrl: ['', Validators.required],
+      description: ['', Validators.required],
+      ingredients: this.fb.array([]),
     });
-    console.log(ingredientsFormsArray);
-    console.log(this.ingredients);
+
+    if (this.editMode) {
+      this.initFromRecipe(this.recipe);
+    }
+  }
+
+  initFromRecipe(recipe: Recipe) {
+    if (recipe.ingredients) {
+      const ingsTemp = recipe.ingredients.slice().map(i => this.fb.group({name: [i.name, Validators.required], amount: [i.amount, [Validators.required,Validators.pattern(/^[1-9]+[0-9]*$/)]]}));
+      const ingredientsFormsArray = this.fb.array(ingsTemp);
+      this.recipeForm.setControl('ingredients', ingredientsFormsArray);
+      this.recipeForm.patchValue({
+        name: recipe.name,
+        imageUrl: recipe.imagePath,
+        description: recipe.description,
+      });
+      console.log(ingredientsFormsArray);
+      console.log(this.ingredients);
+    }
+  }
+
+  get recipe() {
+    return this.recipeService.getRecipeById(this.id);
   }
 
 
