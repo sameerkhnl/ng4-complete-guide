31b43f64c025d9548e1109747a8740134bd8f414
Sameer Khanal <khanal.sam91@gmail.com>
2018-07-06 18:44:31 +1200

    added auth token to interceptor for request

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -4,11 +4,21 @@ import {Observable} from 'rxjs';
 import {HttpHandler} from '@angular/common/http/src/backend';
 import {HttpRequest} from '@angular/common/http/src/request';
 import {Injectable} from '@angular/core';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor{
+
+
+  constructor(private authService: AuthService){
+
+  }
+
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted!', request);
-    return next.handle(request);
+    const copiedRequest = request.clone({params: request.params.append('auth', this.authService.token)});
+
+    return next.handle(copiedRequest);
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams, HttpRequest} from '@angular/common/http';
 import {RecipeService} from '../recipe/recipe.service';
 import {Recipe} from '../recipe/recipe.model';
-import {Observable} from 'rxjs';
 import {AuthService} from '../auth/auth.service';
 
 @Injectable()
@@ -17,13 +16,13 @@ export class DataStorageService {
     const recipes = this.recipeService.getAllRecipes();
     const token = this.authService.getToken();
     //return this.http.put(this.baseUrl, recipes);
-    const req = new HttpRequest('PUT', this.baseUrl, recipes, {reportProgress: true, params: new HttpParams().set('auth', token)});
+    const req = new HttpRequest('PUT', this.baseUrl, recipes, {reportProgress: true, params: new HttpParams()});
     return this.http.request(req);
   }
 
   getRecipes(){
     const token = this.authService.getToken();
-    this.http.get<Recipe[]>(this.baseUrl, {params: new HttpParams().set('auth', token)}).subscribe((recipes: Recipe[]) =>{
+    this.http.get<Recipe[]>(this.baseUrl, {params: new HttpParams()}).subscribe((recipes: Recipe[]) =>{
       for(let recipe of recipes){
         if(!recipe['ingredients']) {
           recipe['ingrdients'] = [];
