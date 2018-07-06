fda81961e190e65a7483956b5a639a7b84e2856a
Sameer Khanal <khanal.sam91@gmail.com>
2018-07-06 18:23:17 +1200

    made auth param dynamic in put and added option: reportProgress true

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams, HttpRequest} from '@angular/common/http';
 import {RecipeService} from '../recipe/recipe.service';
 import {Recipe} from '../recipe/recipe.model';
 import {Observable} from 'rxjs';
@@ -16,12 +16,14 @@ export class DataStorageService {
   putRecipes() {
     const recipes = this.recipeService.getAllRecipes();
     const token = this.authService.getToken();
-    return this.http.put(this.baseUrl + "?auth=" + token, recipes);
+    //return this.http.put(this.baseUrl, recipes);
+    const req = new HttpRequest('PUT', this.baseUrl, recipes, {reportProgress: true, params: new HttpParams().set('auth', token)});
+    return this.http.request(req);
   }
 
   getRecipes(){
     const token = this.authService.getToken();
-    this.http.get<Recipe[]>(this.baseUrl + "?auth=" + token).subscribe((recipes: Recipe[]) =>{
+    this.http.get<Recipe[]>(this.baseUrl, {params: new HttpParams().set('auth', token)}).subscribe((recipes: Recipe[]) =>{
       for(let recipe of recipes){
         if(!recipe['ingredients']) {
           recipe['ingrdients'] = [];
