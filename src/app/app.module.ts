0c8713b903324b0d7a127d78a1bd61496070ecde
Sameer Khanal <khanal.sam91@gmail.com>
2018-07-06 17:28:03 +1200

    optimised imports on app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
-import {UnlessDirective} from './unless-directive/unless.directive';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
-import {HomeComponent} from './home/home.component';
-import {AppRoutingModule} from './app-routing.module';
-import {RecipeService} from './recipe/recipe.service';
 import {HttpClientModule} from '@angular/common/http';
-import {DataStorageService} from './shared/data-storage.service';
 import {SharedModule} from './shared/shared.module';
 import {AuthModule} from './auth/auth-module/auth.module';
-import {RecipesModule} from './recipe/recipes.module';
 import {CoreModule} from './core/core.module';
 
 @NgModule({
